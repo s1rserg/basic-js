b800e40d030afcc8496ca7755091be221ba4cb2f
s1rserg <sirserg05@gmail.com>
2025-10-07 12:24:25 +0300

    feat(format-phone-number): implement phone number formatting

--- src/format-phone-number/index.ts
+++ src/format-phone-number/index.ts
@@ -17,5 +17,26 @@ import { FormatPhoneNumberFn } from './types';
  * Output: ''
  */
 export const formatPhoneNumber: FormatPhoneNumberFn = (str) => {
-  throw new Error('Not Implemented');
+  const nums = str
+    .split('')
+    .filter((char) => char !== ' ' && Number.isInteger(+char))
+    .join('');
+
+  if (nums.length !== 12) {
+    return '';
+  }
+
+  return `+${nums.slice(0, 2)} (${nums.slice(2, 5)}) ${nums.slice(5, 8)}-${nums.slice(8, 10)}-${nums.slice(10)}`;
+};
+
+export const formatPhoneNumberRegex: FormatPhoneNumberFn = (str) => {
+  const nums = str.replace(/\D/g, '');
+  if (nums.length !== 12) {
+    return '';
+  }
+
+  return nums.replace(
+    /(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})/,
+    '+$1 ($2) $3-$4-$5',
+  );
 };
