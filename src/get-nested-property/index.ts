09dcd3f407942138a5ba6a075e407c440bc79cbf
Serhii Serhiienko <sirserg05@gmail.com>
2025-10-08 17:24:52 +0300

    feat(get-nested-property): implement get nested property function

--- src/get-nested-property/index.ts
+++ src/get-nested-property/index.ts
@@ -16,5 +16,47 @@ import { GetNestedPropertyFn } from './types';
  * Output: undefined
  */
 export const getNestedProperty: GetNestedPropertyFn = (obj, path) => {
-  throw new Error('Not Implemented');
+  let current = obj;
+  const parts = path.split('.');
+
+  for (let i = 0; i < parts.length; i++) {
+    const value = current[parts[i]];
+
+    if (i === parts.length - 1) {
+      return value;
+    }
+
+    if (typeof value !== 'object' || value === null) {
+      return;
+    }
+
+    current = value as Record<string, unknown>;
+  }
+};
+
+export const getNestedPropertyRecursively: GetNestedPropertyFn = (
+  obj,
+  path,
+) => {
+  const parts = path.split('.');
+
+  const getNestedPropertyHelper = (
+    obj: Record<string, unknown>,
+    index: number,
+  ): unknown => {
+    const value = obj[parts[index]];
+    const remainingPath = parts.slice(index + 1);
+
+    if (!remainingPath.length) {
+      return value;
+    }
+
+    if (typeof value !== 'object' || value === null) {
+      return;
+    }
+
+    return getNestedPropertyHelper(value as Record<string, unknown>, index + 1);
+  };
+
+  return getNestedPropertyHelper(obj, 0);
 };
